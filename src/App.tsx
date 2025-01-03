66c4fa11050adf7b08cce68ed0c95ccb612c1d12
feiandxs <yiziyint@gmail.com>
2025-01-03 22:16:16 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Game from './components/Game';
 
 function App() {
+  useEffect(() => {
+    document.title = '声控弹球 | Sound Bounce Ball';
+  }, []);
+
   return (
     <div className="App">
       <Game />
